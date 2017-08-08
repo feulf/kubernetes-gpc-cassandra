ab058a182efdaab0993957568ba55a8f9a328699
Federico Ulfo <feulf@users.noreply.github.com>
2017-08-08 12:10:47 -0400

    Delete install-cassandra.sh

--- install-cassandra.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcloud compute disks create --size=200GB --zone=us-east1-b cassandra-disk
-kc apply -f cassandra-pv.yml
-kc apply -f cassandra-sc.yaml
-kc apply -f cassandra-pvc.yaml
-kc apply -f cassandra-service.yaml
-kc apply -f cassandra-peer-service.yaml
-kc apply -f cassandra-deployment.yaml
